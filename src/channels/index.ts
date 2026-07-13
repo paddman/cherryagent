d785f98e9dbe6377e36fc9bfbe7882cd752028c4
paddman <poringhost@gmail.com>
2026-07-13 12:00:53 +0700

    feat(channels): export channel gateway API

---
+++ src/channels/index.ts
@@ -0,0 +1,17 @@
+export { ChannelGateway, type ChannelGatewayOptions } from "./ChannelGateway.js";
+export { getChannelAdapterStatus, type ChannelAdapter } from "./ChannelAdapter.js";
+export type {
+  ChannelAdapterStatus,
+  ChannelAttachment,
+  ChannelCapability,
+  ChannelGatewayMessageResult,
+  ChannelGatewayMessageStatus,
+  ChannelGatewayWebhookResult,
+  ChannelHandlerResult,
+  ChannelMessageHandler,
+  ChannelMetadata,
+  ChannelSendResult,
+  ChannelWebhookRequest,
+  InboundChannelMessage,
+  OutboundChannelMessage,
+} from "./types.js";
